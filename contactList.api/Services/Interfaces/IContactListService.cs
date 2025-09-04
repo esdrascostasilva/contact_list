26bf75edaef19bb7d3d663fd142a82ee3f96619b
esdrascosta <esdras.cts@outlook.com>
2025-09-04 18:14:18 -0300

    fix: corrindo o retorno do metodo que retorna pelo nome

--- contactList.api/Services/Interfaces/IContactListService.cs
+++ contactList.api/Services/Interfaces/IContactListService.cs
@@ -6,7 +6,7 @@ namespace contactList.api.Services.Interfaces;
 public interface IContactListService
 {
     Task<List<ContactPerson>> GetAllContactsAsync();
-    Task<ContactPerson> GetContactByNameAsync(string name);
+    Task<List<ContactPerson>> GetContactByNameAsync(string name);
     Task<ResponseContactPerson> CreateContactAsync(ContactPerson contactPerson);
     Task<bool> UpdateContactAsync(Guid id, ContactPerson requestContactPerson);
     Task<bool> DeleteContactAsync(Guid id);
